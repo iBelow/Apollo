25201a50f52351caeb777893b4956731ef4b0455
iBelow <archi.below@yandex.ru>
2021-01-08 21:49:48 +0300

    updated weather api key

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d25';
+const key = '5dfa1d8dcf4fc4e9802e2ab000b60b2c';
 
 // Set Position function
 setPosition();
